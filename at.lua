22fcee7e787c9cf45656cc04870eb9abcbd29522
edisondorado <58760999+edisondorado@users.noreply.github.com>
2022-04-22 23:18:17 +0600

    Add files via upload

--- at.lua
+++ at.lua
@@ -431,6 +431,8 @@ local sesreport = 0
 local dayFull = ini.onDay.full
 local weekFull = ini.onWeek.full
 isfrac = false
+
+
 local quitReason = { 
 	"Crash",
 	"/q",
@@ -1575,7 +1577,7 @@ function main()
         printStringNow("Updated",2000)
       end
     end)
-
+    autoupdate("https://github.com/edisondorado/arizonatools/raw/main/infoupdate", '['..string.upper(thisScript().name)..']: ', "https://vk.com/tools19")
     --            Команды
     sampRegisterChatCommand('amenu', function() amenu[0] = not amenu[0] end)
     sampRegisterChatCommand('hgps', function() gps_window[0] = not gps_window[0] end)
@@ -6302,6 +6304,61 @@ end
 
 --                             Системки
 
+  function autoupdate(json_url, prefix, url)
+    local dlstatus = require('moonloader').download_status
+    local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+    if doesFileExist(json) then os.remove(json) end
+    downloadUrlToFile(json_url, json,
+      function(id, status, p1, p2)
+        if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+          if doesFileExist(json) then
+            local f = io.open(json, 'r')
+            if f then
+              local info = decodeJson(f:read('*a'))
+              updatelink = info.updateurl
+              updateversion = info.latest
+              f:close()
+              os.remove(json)
+              if updateversion ~= thisScript().version then
+                lua_thread.create(function(prefix)
+                  local dlstatus = require('moonloader').download_status
+                  local color = -1
+                  sampAddChatMessage(tag..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion, -1)
+                  wait(250)
+                  downloadUrlToFile(updatelink, thisScript().path,
+                    function(id3, status1, p13, p23)
+                      if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+                        print(string.format('Загружено %d из %d.', p13, p23))
+                      elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+                        print('Загрузка обновления завершена.')
+                        sampAddChatMessage(tag..'Обновление завершено!', -1)
+                        goupdatestatus = true
+                        lua_thread.create(function() wait(500) thisScript():reload() end)
+                      end
+                      if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+                        if goupdatestatus == nil then
+                          sampAddChatMessage(tag..'Обновление прошло неудачно. Запускаю устаревшую версию..', -1)
+                          update = false
+                        end
+                      end
+                    end
+                  )
+                  end, prefix
+                )
+              else
+                update = false
+                print('v'..thisScript().version..': Обновление не требуется.')
+              end
+            end
+          else
+            print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url)
+            update = false
+          end
+        end
+      end
+    )
+    while update ~= false do wait(100) end
+  end
   function checkIntable(t, str)
     for k, v in pairs(t) do
       if v == str then return true end
